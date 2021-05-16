852f3d6a51490df32a8bb85ceabd8e510ccd52d1
Hiroshimahideyuki <84165155+Hiroshimahideyuki@users.noreply.github.com>
2021-05-16 23:07:28 +0900

    Add files via upload

---
+++ 問題３.R
@@ -0,0 +1,9 @@
+#アヤメデータ
+head(iris, n = 3)
+
+library(tidyverse)
+library(ggplot2)
+
+p_box <- ggplot (data = iris, mapping = aes(x = Species, y =Sepal.Width))+ geom_boxplot() + labs(title = "箱ひげ図")
+
+p_box
