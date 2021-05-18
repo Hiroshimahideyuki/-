dfd4c8731e8380fa52490b865ab3453bed9af218
Hiroshimahideyuki <84165155+Hiroshimahideyuki@users.noreply.github.com>
2021-05-18 13:01:19 +0900

    Update 問題４.R

--- 問題４.R
+++ 問題４.R
@@ -1,3 +1,5 @@
 library(ggplot2)
 
-ggplot(iris, aes(x = Sepal.Width, y = Sepal.Length, color = Species)) + geom_point() +labs(title = "散布図")
+ggplot(iris, aes(x = Sepal.Width, y = Sepal.Length, color = Species))
++ geom_point() 
++labs(title = "散布図")
