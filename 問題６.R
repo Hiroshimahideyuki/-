f702b4318f9938789359698a938e8fdf3ba3ac2a
Hiroshimahideyuki <84165155+Hiroshimahideyuki@users.noreply.github.com>
2021-05-18 12:59:04 +0900

    Update 問題６.R

--- 問題６.R
+++ 問題６.R
@@ -1,11 +1,10 @@
-library(tidyverse)
 library(ggplot2)
 
-data_frame <- data.frame(
-  col1 = 1:100,
-  col2 = rnorm(n = 100, mean = 0, sd = 1)
+data_frame <- data.frame(　#データの格納
+  col1 = 1:100,　#
+  col2 = rnorm(n = 100, mean = 0, sd = 1) #乱数を作成,n=個数, mean=平均, sd=標準偏差
 )
 
 
 
-ggplot(data_frame, aes(x = col1, y = col2)) + geom_line()
+ggplot(data_frame, aes(x = col1, y = col2)) + geom_line()　#折れ線グラフ
