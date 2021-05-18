d39639ff7e2fc0ef8cf3ebdc5eea9cc7a5074c49
Hiroshimahideyuki <84165155+Hiroshimahideyuki@users.noreply.github.com>
2021-05-18 13:00:36 +0900

    Update 問題３.R

--- 問題３.R
+++ 問題３.R
@@ -1,5 +1,7 @@
 library(ggplot2) #ggplot2のパッケージの読み込み
 
-p_box <- ggplot (data = iris, mapping = aes(x = Species, y =Sepal.Width))+ geom_boxplot() + labs(title = "箱ひげ図")
+p_box <- ggplot (data = iris, mapping = aes(x = Species, y =Sepal.Width)) #x軸, y軸の設定
++ geom_boxplot()　#箱ひげ図
++ labs(title = "箱ひげ図")　#名称付け
 
 p_box　#箱ひげ図を表示
