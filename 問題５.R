0adadfb96e13f5016a53be9e2b3d8759d538944b
Hiroshimahideyuki <84165155+Hiroshimahideyuki@users.noreply.github.com>
2021-05-18 12:54:51 +0900

    Update 問題５.R

--- 問題５.R
+++ 問題５.R
@@ -1,7 +1,10 @@
 library(ggplot2)
 
-fish <- read.csv("fish.csv")
+fish <- read.csv("fish.csv")　#fishデータの読み込み
 
 
 
-ggplot(data = fish, mapping = aes(x = length, y = ..density..)) + geom_histogram(alpha = 0.5, bins = 20) + geom_density(size = 1.5) + labs(title = "データの重ね合わせ")
+ggplot(data = fish, mapping = aes(x = length, y = ..density..)) #density=密度
++ geom_histogram(alpha = 0.5, bins = 20) 　#alpha=色の濃さ,　bins=ヒストグラムの階級
++ geom_density(size = 1.5) #size=線の太さ
++ labs(title = "データの重ね合わせ")
