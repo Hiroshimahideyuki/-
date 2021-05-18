7eefa4f0ee35d61db1daa08ebeda38849737ca31
Hiroshimahideyuki <84165155+Hiroshimahideyuki@users.noreply.github.com>
2021-05-18 11:47:57 +0900

    Update 問題５.R

--- 問題５.R
+++ 問題５.R
@@ -4,4 +4,4 @@ fish <- read.csv("fish.csv")
 
 
 
-ggplot(data = fish, mapping = aes(x = length, y = ..density..)) + geom_histogram(alpha = 0.5, bins = 20) + geom_density(size = 1.5) + labs(title = "$B%0%i%U$N=E$M9g$o$;(B")
+ggplot(data = fish, mapping = aes(x = length, y = ..density..)) + geom_histogram(alpha = 0.5, bins = 20) + geom_density(size = 1.5) + labs(title = "データの重ね合わせ")
